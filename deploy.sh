47694ae01b41fdaa994c453baf12ce487d280c88
Martin Jäger <martin@libre.solar>
2021-05-26 08:38:59 +0200

    Switch from Travis CI to GH Actions

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
-# SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (c) 2020 Martin Jäger
-#
-# This script deploys master branch commits to gh-pages root directory and other branches to
-# sub-directory /branches/<branch-name>.
+# This script deploys main branch commits to gh-pages root directory and other branches to
+# sub-directory /branch/<branch-name>.
 #
 # Rendered branches can be accessed via https://libre.solar/thingset/branch/<branch-name>
 #
@@ -13,18 +9,15 @@
 # https://docs.travis-ci.com/user/environment-variables/
 
 
+BRANCH_NAME=$(echo ${GITHUB_REF#refs/heads/})
+
 # adjust link/directory settings in vuepress
-if [ $TRAVIS_PULL_REQUEST != "false" ]; then
-    printf "\nPreparing deployment for pull-request #$TRAVIS_PULL_REQUEST\n"
-    sed -i -e "s/base: '\/thingset\/'/base: '\/thingset\/pr\/$TRAVIS_PULL_REQUEST\/'/g" docs/.vuepress/config.js
-    sed -i -e "s^docsRepo: 'LibreSolar/thingset'^docsRepo: '$TRAVIS_PULL_REQUEST_SLUG'^g" docs/.vuepress/config.js
-    sed -i -e "s/docsBranch: 'master'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
-elif [ $TRAVIS_BRANCH != "master" ]; then
-    printf "\nPreparing deployment for branch $TRAVIS_BRANCH\n"
-    sed -i -e "s/base: '\/thingset\/'/base: '\/thingset\/branch\/$TRAVIS_BRANCH\/'/g" docs/.vuepress/config.js
-    sed -i -e "s/docsBranch: 'master'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
+if [ $BRANCH_NAME != "main" ]; then
+    printf "\nPreparing deployment for branch $BRANCH_NAME\n"
+    sed -i -e "s/base: '\/'/base: '\/branch\/$BRANCH_NAME\/'/g" docs/.vuepress/config.js
+    sed -i -e "s/docsBranch: 'master'/docsBranch: '$BRANCH_NAME'/g" docs/.vuepress/config.js
 else
-    printf "\nPreparing deployment for master\n"
+    printf "\nPreparing deployment for main branch\n"
 fi
 
 # build the website
@@ -36,17 +29,12 @@ rm -rf gh-pages
 git clone -b gh-pages https://github.com/LibreSolar/thingset gh-pages
 
 # compile folders correctly
-if [ $TRAVIS_PULL_REQUEST != "false" ]; then
-    mkdir -p gh-pages/pr
-    rm -rf "gh-pages/pr/$TRAVIS_PULL_REQUEST"
-    cp -r docs/.vuepress/dist "gh-pages/pr/$TRAVIS_PULL_REQUEST"
-elif [ $TRAVIS_BRANCH != "master" ]; then
+if [ $BRANCH_NAME != "main" ]; then
     mkdir -p gh-pages/branch
-    rm -rf "gh-pages/branch/$TRAVIS_BRANCH"
-    cp -r docs/.vuepress/dist "gh-pages/branch/$TRAVIS_BRANCH"
+    rm -rf "gh-pages/branch/$BRANCH_NAME"
+    cp -r docs/.vuepress/dist "gh-pages/branch/$BRANCH_NAME"
 else
     if [ -d "gh-pages/branch" ]; then
-        mv gh-pages/pr docs/.vuepress/dist/
         mv gh-pages/branch docs/.vuepress/dist/
     fi
     rm -rf gh-pages
